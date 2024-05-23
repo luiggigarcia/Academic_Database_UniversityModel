6b42c2a115f5c644f76264afe179e487cbfffbee
LUIGGI PASCHOALINI GARCIA <uniflgarcia@feinet.fei.edu.br>
2024-05-22 22:00:42 -0300

    update sql code

--- sql_code/dql.sql
+++ sql_code/dql.sql
@@ -1,3 +1,4 @@
+-- Historico escolar de todos os alunos.
 select a.nome as "aluno", h.cod_disc as "cod disc", d.nome as "disciplina",
 h.semestre, h.ano, h.nota
 from historico_escolar h
@@ -6,6 +7,7 @@ on a.ra = h.id_aluno
 join disciplina d
 on d.codigo = h.cod_disc;
 
+-- Historico de disciplinas ministradas pelos professores.
 select p.nome as "professor", d.nome as "disciplina" ,h.semestre, h.ano
 from professor p
 join historico_disc_professor h
@@ -13,18 +15,34 @@ on p.id = h.id_prof
 join disciplina d
 on d.codigo = h.cod_disc;
 
-select a.ra as "id_aluno", a.nome as "aluno"
-from historico_escolar h
-join matriz_curricular m
-on m.id_disc = h.cod_disc and h.nota >= 5 and h.semestre = 4 and h.ano = 2020
-join aluno a
-on a.ra = h.id_aluno;
+-- Todos os alunos que se formaram.
+with alunos_formandos as (
+	select distinct a.ra as "id_aluno", a.nome as "aluno", h.semestre, h.ano, d.nome as "disc", h.nota,
+	h.nota >= 5 as "aprovado"
+	from matriz_curricular m
+	left join historico_escolar h
+	on m.id_disc = h.cod_disc
+	inner join aluno a
+	on a.ra = h.id_aluno
+	join disciplina d
+	on d.codigo = h.cod_disc
+	order by a.ra , h.ano, h.semestre
+), formados as (
+	select id_aluno, aluno, count(case when "aprovado" then 1 end) = count(disc) as "formado"
+	from alunos_formandos
+	group by id_aluno, aluno
+)
+select id_aluno, aluno, formado
+from formados 
+where formado;
 
+-- Professores chefes de departamento e o nome do dept.
 select p.nome as "chefe_dep", d.nome as "dept"
 from departamento d
 inner join professor p
 on d.chefe_dept = p.id;
 
+-- Alunos que fazem parte de um grupo de tcc e o seu orientador.
 select t.id as "id_tcc", a.nome as "aluno", p.nome as "orientador"
 from tcc t
 join grupo_tcc g
@@ -32,5 +50,6 @@ on t.id = g.id_tcc
 join aluno a 
 on a.ra = g.id_aluno
 join professor p
-on p.id = t.id_prof;
+on p.id = t.id_prof
+order by id_tcc;
 
